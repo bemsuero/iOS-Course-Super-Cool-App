ec468567b27d4b42952b6a414455c59821f2df48
Bem <BemSuero@miltons-mbp.fios-router.home>
2016-06-07 06:09:01 -0400

    Initial Commit

---
+++ Cool App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cool App
+//
+//  Created by Bem on 6/7/16.
+//  Copyright © 2016 Bem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
